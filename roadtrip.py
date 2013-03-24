ca3e1261da1970906509fc93ff9fad977689caa9
Sam Johnson <samuel.johnson@gmail.com>
2013-03-24 15:32:44 -0600

    pep8 cleanup

--- roadtrip.py
+++ roadtrip.py
@@ -12,72 +12,74 @@ ch.setFormatter(log_formatter)
 
 log.addHandler(ch)
 
-class RoadTripVehicleFuelRecord:
-	def __init__(self, csvRow):
-		values = csvRow.split(",")
 
-		self.Odometer 		= values[0]
-		self.TripDistance 	= values[1]
-		dateValue			= values[2].split(" ")[0].replace('"','')
-		(year,month,day) 	= dateValue.split('-')
-		self.Date 			= datetime.date(int(year),int(month),int(day))
-		self.FillAmount 	= values[3]
-		self.FillUnits 		= values[4]
-		self.PricePerUnit 	= values[5]
-		self.TotalPrice 	= values[6]
-		self.PartialFill 	= values[7]
+class RoadTripVehicleFuelRecord:
+    def __init__(self, csvRow):
+        values = csvRow.split(",")
+
+        self.Odometer     = values[0]
+        self.TripDistance = values[1]
+        dateValue         = values[2].split(" ")[0].replace('"', '')
+        year, month, day  = dateValue.split('-')
+        self.Date         = datetime.date(int(year), int(month), int(day))
+        self.FillAmount   = values[3]
+        self.FillUnits    = values[4]
+        self.PricePerUnit = values[5]
+        self.TotalPrice   = values[6]
+        self.PartialFill  = values[7]
+
 
 class RoadTripTireLogRecord:
-	def __init__(self, csvRow):
-		values = csvRow.split(",")
-		self.Name 		= values[0]
-		year,month,day 	= values[1].replace('"','').split('-')
-		self.StartDate 	= datetime.date(int(year),int(month),int(day))
-		self.StartOdometer = values[2]
-		self.Distance = values[6]
+    def __init__(self, csvRow):
+        values             = csvRow.split(",")
+        self.Name          = values[0]
+        year, month, day   = values[1].replace('"', '').split('-')
+        self.StartDate     = datetime.date(int(year), int(month), int(day))
+        self.StartOdometer = values[2]
+        self.Distance      = values[6]
+
 
 class RoadTripVehicle:
-	def __init__(self, csvFile):
-		
-		# Instance Variables
-		self._section = ""
-		self.Version = ""
-		self.Language = ""
-		self.FuelRecords = []
-		self.TireLogRecords = []
+    def __init__(self, csvFile):
+        # Instance Variables
+        self._section       = ""
+        self.Version        = ""
+        self.Language       = ""
+        self.FuelRecords    = []
+        self.TireLogRecords = []
 
-		# Logging
-		log = logging.getLogger(__name__)
+        # Logging
+        log = logging.getLogger(__name__)
 
-		self._i = 0
-		for line in csvFile:
-			self._i+=1
-			line = line.strip()
-			if self._section and not line:
-				self._section = line
-				log.debug(str(self._i)+": clearing section")
-				continue
+        self._i = 0
+        for line in csvFile:
+            self._i += 1
+            line = line.strip()
+            if self._section and not line:
+                self._section = line
+                log.debug(str(self._i) + ": clearing section")
+                continue
 
-			if line in ("Version, Language", "FUEL RECORDS", "TIRE LOG"):
-				self._section = line
-				log.debug(str(self._i)+": found " + self._section + " Section")
-				continue
+            if line in ("Version, Language", "FUEL RECORDS", "TIRE LOG"):
+                self._section = line
+                log.debug(str(self._i) + ": found section:" + self._section)
+                continue
 
-			if "Version,Language" in self._section:
-				values = line.split(",")
-				self.Version = values[0]
-				self.Language = values[1]
-				log.debug(str(self._i) + ": Saving Version/Language values")
-				continue
+            if "Version,Language" in self._section:
+                values = line.split(",")
+                self.Version = values[0]
+                self.Language = values[1]
+                log.debug(str(self._i) + ": Saving Version/Language values")
+                continue
 
-			if "FUEL RECORDS" in self._section:
-				if "Odometer" not in line:
-					self.FuelRecords.append( RoadTripVehicleFuelRecord(line) )
-					log.debug(str(self._i) + ": added " + self._section)
-				continue
+            if "FUEL RECORDS" in self._section:
+                if "Odometer" not in line:
+                    self.FuelRecords.append(RoadTripVehicleFuelRecord(line))
+                    log.debug(str(self._i) + ": added " + self._section)
+                continue
 
-			if "TIRE LOG" in self._section:
-				if "Name" not in line:
-					self.TireLogRecords.append( RoadTripTireLogRecord(line) )
-					log.debug(str(self._i) + ": added " + self._section)
-				continue
+            if "TIRE LOG" in self._section:
+                if "Name" not in line:
+                    self.TireLogRecords.append(RoadTripTireLogRecord(line))
+                    log.debug(str(self._i) + ": added " + self._section)
+                continue
